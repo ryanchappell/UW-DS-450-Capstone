3c5e9eebfa70f7df84ec0d1b6159d41d1f6a9c2b
Ryan <ryan.p.chap@gmail.com>
2016-08-18 17:25:39 -0700

    more cleanup in prep for training/test data

--- prepareData.R
+++ prepareData.R
@@ -30,40 +30,9 @@ if (interactive()) {
   loginfo(paste0('maxEventsToRead is ',maxEventsToRead, ', maxAppEventsToRead is ',maxAppEventsToRead))
   #loginfo(paste0('maxAppEventsToRead is ',maxAppEventsToRead))
 
-  loginfo('Reading events.csv')
-  
-  events_csv = read.csv('data/events.csv', header = TRUE, 
-                          nrows = maxEventsToRead, 
-                          numerals = 'warn.loss',
-                          # use character class as we would otherwise lose precision
-                          # (using 'numeric') with the size of device_id values
-                          colClasses = c('character', 'character', 'POSIXct',NA,NA))
-  
-  loginfo('Reading gender_age_train.csv')
-  gender_age_train_csv = read.csv('data/gender_age_train.csv', 
-                       numerals = 'warn.loss',
-                       # use character class as we would otherwise lose precision
-                       # (using 'numeric') with the size of device_id values
-                       colClasses = c('character','factor', NA, 'factor'))
-  
-  loginfo('Getting intersection between gender_age_train_csv and events_csv on device_id')
-  intersectDeviceId = intersect(gender_age_train_csv$device_id, events_csv$device_id)
-  
-  loginfo('Omitting events_csv where device_id does not exist in gender_age_train_csv')
-  events_csv = events_csv[events_csv$device_id %in% intersectDeviceId,]
-  
-  # add is_weekend flag
-  events_csv$isWeekend = getIsWeekend(events_csv$timestamp)
-  # add day of week feature
-  events_csv$dow = getDow(events_csv$timestamp)
-  # add time window feature (e.g. "morning", "afternoon")
-  events_csv$timeWindow = getTimeWindow(events_csv$timestamp)
-  # add hour of day
-  events_csv$hour = getHour(events_csv$timestamp)
-  
   loginfo('Reading label_categories.csv')
   label_categories_csv = read.csv('data/label_categories.csv',
-                             numerals = 'warn.loss')
+                                  numerals = 'warn.loss')
   
   # TODO: the consolidateCategories function is not done, 
   # come back if you have time (low priority)
@@ -72,31 +41,74 @@ if (interactive()) {
   
   loginfo('Reading app_labels.csv')
   app_labels_csv = read.csv('data/app_labels.csv',
-                       numerals = 'warn.loss',
-                       # use character class as we would otherwise lose precision
-                       # (using 'numeric') with the size of app_id values
-                       colClasses = c('character'))
-
+                            numerals = 'warn.loss',
+                            # use character class as we would otherwise lose precision
+                            # (using 'numeric') with the size of app_id values
+                            colClasses = c('character'))
+  
   
   loginfo('Flatten relationship  (app category)')
-  appCategories = merge(app_labels_csv, label_categories_csv, by = "label_id", all.x = TRUE)
-
+  mergedData = merge(app_labels_csv, label_categories_csv, by = "label_id", all.x = TRUE)
+  
   loginfo('Reading app_events.csv')
   app_events_csv = read.csv('data/app_events.csv', 
-                       numerals = 'warn.loss',
-                       nrows = maxAppEventsToRead,
-                       # use character class as we would otherwise lose precision
-                       # (using 'numeric') with the size of app_id values
-                       colClasses = c(NA,'character', 'factor', 'factor'))
+                            numerals = 'warn.loss',
+                            nrows = maxAppEventsToRead,
+                            # use character class as we would otherwise lose precision
+                            # (using 'numeric') with the size of app_id values
+                            colClasses = c(NA,'character', 'factor', 'factor'))
   
   loginfo('Flatten relationship (app events and app categories)')
-  appEventCategories = merge(app_events_csv, appCategories, by = "app_id", all.x = TRUE)
+  mergedData = merge(app_events_csv, mergedData, by = "app_id", all.x = TRUE)
+  
+  loginfo('Reading events.csv')
+  events_csv = read.csv('data/events.csv', header = TRUE, 
+                          nrows = maxEventsToRead, 
+                          numerals = 'warn.loss',
+                          # use character class as we would otherwise lose precision
+                          # (using 'numeric') with the size of device_id values
+                          colClasses = c('character', 'character', 'POSIXct',NA,NA))
   
   loginfo('Flatten relationship (device events and app events)');
-  eventData = merge(events_csv, appEventCategories, by = "event_id")
+  mergedData = merge(events_csv, mergedData, by = "event_id")
+  
+  loginfo('Reading phone_brand_device_model.csv')
+  # TODO: review character encoding, e.g. values like 'å°ç±³' for phone_brand column
+  phone_brand_device_model_csv = read.csv('data/phone_brand_device_model.csv', 
+                                          encoding="UTF-8", 
+                                          numerals = 'warn.loss',
+                                          # use character class as we would otherwise lose precision
+                                          # (using 'numeric') with the size of device_id values
+                                          colClasses = c('character','factor','factor'))
+  
+  loginfo('Flatten relationship (gender/age and phone specs)')
+  mergedData = merge(mergedData, phone_brand_device_model_csv, by = "device_id")
+  
+  loginfo('Reading gender_age_train.csv')
+  gender_age_train_csv = read.csv('data/gender_age_train.csv', 
+                       numerals = 'warn.loss',
+                       # use character class as we would otherwise lose precision
+                       # (using 'numeric') with the size of device_id values
+                       colClasses = c('character','factor', NA, 'factor'))
+  
+  loginfo('Getting intersection between gender_age_train_csv and mergedData on device_id')
+  intersectDeviceId = intersect(gender_age_train_csv$device_id, mergedData$device_id)
+  
+  loginfo('Omitting mergedData where device_id does not exist in gender_age_train_csv')
+  mergedData = mergedData[mergedData$device_id %in% intersectDeviceId,]
+  
+  # add is_weekend flag
+  mergedData$isWeekend = getIsWeekend(mergedData$timestamp)
+  # add day of week feature
+  mergedData$dow = getDow(mergedData$timestamp)
+  # add time window feature (e.g. "morning", "afternoon")
+  mergedData$timeWindow = getTimeWindow(mergedData$timestamp)
+  # add hour of day
+  mergedData$hour = getHour(mergedData$timestamp)
+
 
   loginfo('Flatten relationship (gender/age and phone event data)')
-  genderAgeDevice = merge(gender_age_train_csv, eventData, by = "device_id")
+  genderAgeDevice = merge(gender_age_train_csv, mergedData, by = "device_id")
 
   
   loginfo('Reading phone_brand_device_model.csv')
@@ -109,31 +121,31 @@ if (interactive()) {
                         colClasses = c('character','factor','factor'))
   
   loginfo('Flatten relationship (gender/age and phone specs)')
-  flatData = merge(genderAgeDevice, phone_brand_device_model_csv, by = "device_id", all.x = TRUE)
+  mergedData = merge(genderAgeDevice, phone_brand_device_model_csv, by = "device_id", all.x = TRUE)
   
   loginfo('Writing flattened data')
-  write.csv(flatData, 'output/flatData.csv')
+  write.csv(mergedData, 'output/mergedData.csv')
   
   loginfo('Removing some columns from flattened data')
-  flatNarrowData = data.frame(flatData)
-  flatNarrowData$event_id = NULL
-  #flatNarrowData$app_id = NULL
-  flatNarrowData$label_id = NULL
-  #flatNarrowData$is_active = NULL
-  flatNarrowData$longitude = NULL
-  flatNarrowData$latitude = NULL
-  flatNarrowData$timestamp = NULL
-  flatNarrowData$is_installed = NULL
-  flatNarrowData$gender = NULL
-  flatNarrowData$age = NULL
-  flatNarrowData$category = NULL
+  mergedDataNarrow = data.frame(mergedData)
+  mergedDataNarrow$event_id = NULL
+  #mergedDataNarrow$app_id = NULL
+  mergedDataNarrow$label_id = NULL
+  #mergedDataNarrow$is_active = NULL
+  mergedDataNarrow$longitude = NULL
+  mergedDataNarrow$latitude = NULL
+  mergedDataNarrow$timestamp = NULL
+  mergedDataNarrow$is_installed = NULL
+  mergedDataNarrow$gender = NULL
+  mergedDataNarrow$age = NULL
+  mergedDataNarrow$category = NULL
   
   # de-duplicate
-  flatNarrowData = unique(flatNarrowData)
+  mergedDataNarrow = unique(mergedDataNarrow)
   
   loginfo('Writing flattened, narrowed data (omitting some columns)')
-  write.csv(flatNarrowData, 'output/flatNarrowData.csv')
+  write.csv(mergedDataNarrow, 'output/mergedDataNarrow.csv')
 
-  plotAgeGroupPopularPhoneBrands(flatNarrowData)
-  plotAgeGroupPopularDeviceModels(flatNarrowData)
+  #plotAgeGroupPopularPhoneBrands(mergedDataNarrow)
+  #plotAgeGroupPopularDeviceModels(mergedDataNarrow)
 }
