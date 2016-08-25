4815f591de3252f6d80dd9a91d6893fb0cca57e6
ryanchappell <ryan.p.chap@gmail.com>
2016-08-24 22:09:16 -0700

    correcting merge and rbind

--- roughDeviceCategoryBatch.R
+++ roughDeviceCategoryBatch.R
@@ -6,7 +6,7 @@ source('classifyAppCategories.R')
 
 # rough amount of lines of app_events.csv
 appEventLinesRemaining = 32000000
-batchSize = 1000000
+batchSize = 100000
 eventDeviceMap = getEventDeviceMap(FALSE)
 appCategoryCounts = getAppCategoryCounts()
 
@@ -14,11 +14,11 @@ appCategoryCounts = getAppCategoryCounts()
 colNames = c("event_id", "app_id", "is_installed","is_active")
 currentBatchNumber = 0
 
-deviceAppEventCategories = data.frame()
+deviceAppEventCategories = NULL
+combinedRows = NULL
+keepReading = TRUE
 
-stopReading = FALSE
-
-while(stopReading){
+while(keepReading){
   loginfo(paste0('Reading data/app_events.csv, appEventLinesRemaining: ',appEventLinesRemaining, 
                  ' batchSize: ', batchSize, 
                  ', currentBatchNumber:', currentBatchNumber))
@@ -32,57 +32,37 @@ while(stopReading){
                             colClasses = c(NA,'character', 'integer', 'integer'), 
                             col.names = colNames)
   
-  if (nrow(app_events_csv_batch_1) < batchSize){
-    stopReading = TRUE
+  if (nrow(app_events_csv_batch_1) < batchSize){ #|| currentBatchNumber == 0){
+    keepReading = FALSE
   }
-  
-#  app_events_csv_batch_2 = read.csv('data/app_events.csv', 
-#                            numerals = 'warn.loss',
-#                            nrows = batchSize, 
-#                            skip = batchSize, # skip the first batch
-                            # use character class as we would otherwise lose precision
-                            # (using 'numeric') with the size of app_id values
-#                            colClasses = c(NA,'character', 'integer', 'integer'), 
-#                            col.names = colNames)
-  
-  # merge and aggregation for batch 1
+
   mergedEventDeviceCats_batch_1 = merge(eventDeviceMap, app_events_csv_batch_1, by = "event_id")
   mergedEventDeviceCats_batch_1 = merge(mergedEventDeviceCats_batch_1, appCategoryCounts, by = "app_id")
   
   # which columns we are aggregating
-  aggColumns = 4:10
-  #summedDeviceCategories_batch_1 = aggregate(x = mergedEventDeviceCats_batch_1[,aggColumns], 
-  #                                   by = list(device_id = mergedEventDeviceCats_batch_1$device_id), 
-  #                                   FUN = function(x){
-  #                                      sum(x)
-  #                                   })
+  combinedAggColumns = 4:(ncol(mergedEventDeviceCats_batch_1) - 1)
   
-  # merge and aggregation for batch 2
-  #mergedEventDeviceCats_batch_2 = merge(eventDeviceMap, app_events_csv_batch_2, by = "event_id")
-  #mergedEventDeviceCats_batch_2 = merge(mergedEventDeviceCats_batch_2, appCategoryCounts, by = "app_id")
-  
-  #summedDeviceCategories_batch_2 = aggregate(x = mergedEventDeviceCats_batch_2[,aggColumns], 
-  #                                   by = list(device_id = mergedEventDeviceCats_batch_2$device_id), 
-  ##                                   FUN = function(x){
-  #                                     sum(x)
-  #                                   })
-  
-  # okay! combine and sum the categories!
-  #combinedRows = rbind(mergedEventDeviceCats_batch_1, mergedEventDeviceCats_batch_2)
-  combinedRows = rbind(deviceAppEventCategories, mergedEventDeviceCats_batch_1)
-  
-  combinedAggColumns = 4:(ncol(combinedRows) - 1)
-  finalSummed = aggregate(x = combinedRows[,combinedAggColumns], 
-                                     by = list(device_id = combinedRows$device_id), 
+  deviceAppEventCategories = aggregate(x = mergedEventDeviceCats_batch_1[,combinedAggColumns], 
+                                     by = list(device_id = mergedEventDeviceCats_batch_1$device_id), 
                                      FUN = function(x){
                                        sum(x)
                                      })
+
+  if (!is.null(combinedRows)){
+    combinedRows = rbind(deviceAppEventCategories, combinedRows)  
+  } else {
+    combinedRows = deviceAppEventCategories
+  }
+  
+    
+  print(paste0('deviceAppEventCategories row count: ', nrow(combinedRows)))
+  print(paste0('max is_installed: ', max(combinedRows$is_installed),' max is_game: ', max(combinedRows$is_game)))
   
   currentBatchNumber = currentBatchNumber + 1
   appEventLinesRemaining = appEventLinesRemaining - batchSize
 }
 
-write.csv(finalSummed, file = 'adjusted-data/device_app_event_categories.csv')
+write.csv(combinedRows, file = 'adjusted-data/device_app_event_categories.csv')
 
 #sanity check here w/ one of the devices
 # category counts in this one...
